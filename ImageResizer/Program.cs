cd4ef1620648cd163ad81737a5be15c2ba01d17a
Vic <sky40182@gmail.com>
2019-10-04 20:19:31 +0800

    Async

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,21 +9,44 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
 
+            Stopwatch sw1 = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+
             ImageProcess imageProcess = new ImageProcess();
+
+            #region 同步
 
             imageProcess.Clean(destinationPath);
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
+            sw1.Start();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
-            sw.Stop();
+            sw1.Stop();
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"花費時間: {sw1.ElapsedMilliseconds} ms");
+
+            #endregion 同步
+
+            #region 非同步
+
+            imageProcess.Clean(destinationPath);
+
+            sw2.Start();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw2.Stop();
+
+            Console.WriteLine($"花費時間: {sw2.ElapsedMilliseconds} ms");
+
+            #endregion 非同步
+
+            double efficacy = Math.Round(Convert.ToDouble(sw1.ElapsedMilliseconds - sw2.ElapsedMilliseconds) / sw1.ElapsedMilliseconds * 100, 4);
+
+            Console.WriteLine($"提升效能: {efficacy} ");
+            Console.ReadKey();
         }
     }
 }
